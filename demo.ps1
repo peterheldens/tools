c7dcd7623d58c8399e4f1d388213ff2086a1304e
Peter Heldens <peter.heldens@live.nl>
2020-08-11 16:17:02 +0200

    added executin policy

--- demo.ps1
+++ demo.ps1
@@ -1,1 +1,1 @@
-iex ((New-Object System.Net.WebClient).DownloadString('https://github.com/peterheldens/tools/demo/hello.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://github.com/peterheldens/tools/tree/master/demo/hello.ps1'))
