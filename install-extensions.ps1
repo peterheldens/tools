49b896e05d3224acee1b055a021dbe91f2162fc9
Peter Heldens <peter.heldens@live.nl>
2020-08-11 10:48:08 +0200

    added some extensions

--- install-extensions.ps1
+++ install-extensions.ps1
@@ -1,13 +1,40 @@
-code --install-extension tomoki1207.pdf
-code --install-extension CoenraadS.code 
-code --install-extension bracket-pair-colorizer-2
+# My VSCode Extensions
+
+# A customizable extension for colorizing matching brackets
+code --install-extension coenraads.bracket-pair-colorizer-2  
+
+# Open any folder in the Windows Subsystem for Linux (WSL) 
 code --install-extension ms-vscode-remote.remote-wsl
+
+# (Preview) Develop PowerShell scripts in Visual Studio Code!
 code --install-extension ms-vscode.powershell
-code --install-extension tomoki1207.pdf
+
+# Icons for Visual Studio Code
 code --install-extension vscode-icons-team.vscode-icons
+
+# REST Client for Visual Studio Code
 code --install-extension humao.rest-client
+
+# Code formatter using prettier
 code --install-extension esbenp.prettier-vscode
+
+# Makes indentation easier to read
 code --install-extension oderwat.indent-rainbow
+
+# Auto rename paired HTML/XML tag
 code --install-extension formulahendry.auto-rename-tag
+
+# View git log, file history, compare branches or commits
 code --install-extension donjayamanne.githistory
+
+# Supercharge the Git capabilities built into Visual Studio Code 
 code --install-extension eamodio.gitlens
+
+# display pdf file
+code --install-extension tomoki1207.pdf
+
+# markdown lint
+code --install-extension DavidAnson.vscode-markdownlint
+
+# Beautify code in place for VS Code
+code --install-extension hookyqr.beautify
--- uninstall-extensions.ps1
+++ uninstall-extensions.ps1
@@ -1,13 +1,40 @@
-code --uninstall-extension tomoki1207.pdf
-code --uninstall-extension CoenraadS.code 
-code --uninstall-extension bracket-pair-colorizer-2
+# My VSCode Extensions
+
+# A customizable extension for colorizing matching brackets
+code --uninstall-extension coenraads.bracket-pair-colorizer-2  
+
+# Open any folder in the Windows Subsystem for Linux (WSL) 
 code --uninstall-extension ms-vscode-remote.remote-wsl
+
+# (Preview) Develop PowerShell scripts in Visual Studio Code!
 code --uninstall-extension ms-vscode.powershell
-code --uninstall-extension tomoki1207.pdf
+
+# Icons for Visual Studio Code
 code --uninstall-extension vscode-icons-team.vscode-icons
+
+# REST Client for Visual Studio Code
 code --uninstall-extension humao.rest-client
+
+# Code formatter using prettier
 code --uninstall-extension esbenp.prettier-vscode
+
+# Makes indentation easier to read
 code --uninstall-extension oderwat.indent-rainbow
+
+# Auto rename paired HTML/XML tag
 code --uninstall-extension formulahendry.auto-rename-tag
+
+# View git log, file history, compare branches or commits
 code --uninstall-extension donjayamanne.githistory
+
+# Supercharge the Git capabilities built into Visual Studio Code 
 code --uninstall-extension eamodio.gitlens
+
+# display pdf file
+code --uninstall-extension tomoki1207.pdf
+
+# markdown lint
+code --uninstall-extension DavidAnson.vscode-markdownlint
+
+# Beautify code in place for VS Code
+code --uninstall-extension hookyqr.beautify
